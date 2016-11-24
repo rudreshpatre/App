cdaeb3b08e14e4db9a8e41267d3b7e99f1450805
Rudresh Patre <rudreshpatre@gmail.com>
2016-11-25 00:04:13 +0530

    Basic features

---
+++ App/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using App.Models;
+
+namespace App.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Get = hour > 12 ? "Good Evening" : "Good Morning"; 
+            return View();
+        }
+
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestresponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestresponse);
+            }
+            else
+            {
+                return View();
+            }            
+        }
+    }
+}
---
+++ App/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please enter your name.")]
+        public String Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [EmailAddress(ErrorMessage = "Please enter your email address in correct format.")]
+        public String Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone.")]
+        public String Phone { get; set; }
+        [Required(ErrorMessage = "Please make a selection.")]
+        public bool? WillAttend { get; set; }
+    }
+}
